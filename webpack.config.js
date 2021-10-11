569c30a7797cb8683ac903b0c5f5ed408073dcfc
windowdotonload <275981471@qq.com>
2021-10-11 17:27:03 +0800

    feat(handle webpack): handle webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   // entry: {
   //   app: ["./src/index.js"],
   // },
-  entry: path.join(__dirname, "../src/index.js"),
+  entry: path.join(__dirname, "./src/index.js"),
 
   output: {
     path: path.resolve(process.cwd(), "./lib"),
